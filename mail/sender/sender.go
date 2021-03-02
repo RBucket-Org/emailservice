fa4b448e99bbc8e13eba90f359c84fecd6b720a0
egnimos <egnimos25@gmail.com>
2021-03-02 22:14:16 +0530

    template removed

--- mail/sender/sender.go
+++ mail/sender/sender.go
@@ -8,10 +8,6 @@ import (
 	"github.com/mailgun/mailgun-go"
 )
 
-const (
-	rootdir = "C:/Users/91972/Documents/WORKSPACE/emailservice/mail/sender"
-)
-
 //TemplateData : provide the structure of data to the given email
 type TemplateData struct {
 	Name  string
@@ -39,16 +35,16 @@ func ParseTemplate(templateFileName string, data interface{}) (string, error) {
 }
 
 //SendEmailVerification : this method sends the email attach with template to verify the email
-func SendEmailVerification(domain string, apiKey string, template *TemplateData) (string, error) {
+func SendEmailVerification(domain string, apiKey string, template *TemplateData, templateFileName string, rootdir string) (string, error) {
 	mg := mailgun.NewMailgun(domain, apiKey)
 
 	//parse the template
-	msgBody, err := ParseTemplate(filepath.Join(rootdir, "./email-verification.htm"), template)
+	msgBody, err := ParseTemplate(filepath.Join(rootdir, templateFileName), template)
 	if err != nil {
 		return "nil", err
 	}
 
-	//generate the new message by taking the from, subject, email, body will be empty 
+	//generate the new message by taking the from, subject, email, body will be empty
 	m := mg.NewMessage(
 		template.From,
 		"Verify Your Email",
@@ -65,16 +61,16 @@ func SendEmailVerification(domain string, apiKey string, template *TemplateData)
 }
 
 //SendResetPassword : send the reset password mail to the semder
-func SendResetPassword(domain string, apiKey string, template TemplateData) (string, error) {
+func SendResetPassword(domain string, apiKey string, template TemplateData, templateFileName string, rootdir string) (string, error) {
 	mg := mailgun.NewMailgun(domain, apiKey)
 
 	//parse the template
-	msgBody, err := ParseTemplate("./reset-password.gohtml", template)
+	msgBody, err := ParseTemplate(filepath.Join(rootdir, templateFileName), template)
 	if err != nil {
 		return "nil", err
 	}
 
-	//generate the new message by taking the from, subject, email, body will be empty 
+	//generate the new message by taking the from, subject, email, body will be empty
 	m := mg.NewMessage(
 		template.From,
 		"Reset Your Password",
